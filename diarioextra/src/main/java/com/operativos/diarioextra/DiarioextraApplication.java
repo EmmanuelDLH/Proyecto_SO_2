1da359889e04e9bfcb32e2c889a8acc51969c973
Emmanuel <ledezma.emmanuel@gmail.com>
2021-06-02 23:42:51 -0600

    Fourth news portal added

---
+++ diarioextra/src/main/java/com/operativos/diarioextra/DiarioextraApplication.java
@@ -0,0 +1,13 @@
+package com.operativos.diarioextra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiarioextraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiarioextraApplication.class, args);
+	}
+
+}
